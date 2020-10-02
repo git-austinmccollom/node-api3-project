b6028e5fc3f208c24c0df94611425e53ec4915e4
git-austinmccollom <austinmccollom@gmail.com>
2020-10-01 20:09:19 -0500

    validateUser

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,48 +1,52 @@
-const express = require('express');
-const userDbFunctions = require('./userDb');
+const express = require("express");
+const userDbFunctions = require("./userDb");
 
 const router = express.Router();
-  
-router.post('/', (req, res) => {
+
+router.post("/", validateUser, (req, res) => {
   // userDbFunctions.insert({ name: "austin" })
-  console.log(req.body)
-  userDbFunctions.insert(req.body)
-  .then( postRes => {
-    res.status(201).json(postRes);
-  })
-  .catch( postErr => {
-    // res.status(500).json({ message: "server failed to save user" })
-    res.status(500).json({postErr})
-  })
+  console.log(req.body);
+  userDbFunctions
+    .insert(req.body)
+    .then((postRes) => {
+      res.status(201).json(postRes);
+    })
+    .catch((postErr) => {
+      // res.status(500).json({ message: "server failed to save user" })
+      res.status(500).json({ postErr });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
-  userDbFunctions.get()
-  .then( users => {
-    res.status(200).json(users)
-  })
-  .catch( err => {
-    res.status(500).json({ message: "error retrieving users "})
-  })
+router.get("/", (req, res) => {
+  userDbFunctions
+    .get()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "error retrieving users " });
+    });
 });
 
-router.get('/:id', validateUserId, (req, res) => {
-  req.user ? res.status(200).json(req.user) : res.status(404).json({ message: "user not found" });
+router.get("/:id", validateUserId, (req, res) => {
+  req.user
+    ? res.status(200).json(req.user)
+    : res.status(404).json({ message: "user not found" });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
@@ -52,18 +56,27 @@ function validateUserId(req, res, next) {
   userDbFunctions
     .getById(req.params.id)
     .then((user) => {
-      req.user = user
+      req.user = user;
       next();
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err);
       // res.status(404).json({ message: "user not found" });
       res.send("404 not found");
     });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  console.log(req.body);
+  if (req.body) {
+    if (req.body.name) {
+      next();
+    } else {
+      res.status(400).json({ message: "missing required name field" });
+    }
+  } else {
+    res.status(400).json({ message: "missing user data" });
+  }
 }
 
 function validatePost(req, res, next) {
